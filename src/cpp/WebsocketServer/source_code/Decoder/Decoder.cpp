ec172ef27217fa9aa0e108d32e80884f1a3be809
Charles J London <charles.jacob.london@gmail.com>
2020-05-22 15:24:09 -0400

    Replace switch statement with mapping of MessageHandlers...will need to find a better place for these functions - maybe inheritance or message CRTP

--- src/cpp/WebsocketServer/source_code/Decoder/Decoder.cpp
+++ src/cpp/WebsocketServer/source_code/Decoder/Decoder.cpp
@@ -1,12 +1,151 @@
 #include "Decoder.h"
-#include <string>
-#include <utility>
-#include <math.h>
+#include <iostream>
 
 Decoder::Decoder() :
 	symbolMap(std::unordered_map<uint32_t, SymbolIndexMappingMessage>()),
 	mqConnection(AmqpClient::Channel::Create())
 {
+	populateMessageMap();
+}
+
+void Decoder::populateMessageMap()
+{
+	messageHandlerMap.emplace(MessageType::TRADE, [=](char packetData[]) { this->handleTrade(packetData); });
+	messageHandlerMap.emplace(MessageType::SEQUENCE_NUMBER_RESET, [=](char packetData[]) { this->handleTradeSequenceNumberReset(packetData); });
+	messageHandlerMap.emplace(MessageType::SYMBOL_INDEX_MAPPING, [=](char packetData[]) { this->handleSymbolIndexMapping(packetData); });
+	messageHandlerMap.emplace(MessageType::SYMBOL_CLEAR, [=](char packetData[]) { this->handleSymbolClear(packetData); });
+	messageHandlerMap.emplace(MessageType::SECURITY_STATUS, [=](char packetData[]) { this->handleSecurityStatus(packetData); });
+	messageHandlerMap.emplace(MessageType::SOURCE_TIME_REFERENCE, [=](char packetData[]) { this->handleSourceTimeReference(packetData); });
+	messageHandlerMap.emplace(MessageType::QUOTE, [=](char packetData[]) { this->handleQuote(packetData); });
+	messageHandlerMap.emplace(MessageType::ADD_ORDER, [=](char packetData[]) { this->handleAddOrder(packetData); });
+	messageHandlerMap.emplace(MessageType::MODIFY_ORDER, [=](char packetData[]) { this->handleModifyOrder(packetData); });
+	messageHandlerMap.emplace(MessageType::DELETE_ORDER, [=](char packetData[]) { this->handleDeleteOrder(packetData); });
+	messageHandlerMap.emplace(MessageType::REPLACE_ORDER, [=](char packetData[]) { this->handleReplaceOrder(packetData); });
+	messageHandlerMap.emplace(MessageType::TRADE_CANCEL, [=](char packetData[]) { this->handleTradeCancel(packetData); });
+	messageHandlerMap.emplace(MessageType::ORDER_EXECUTION, [=](char packetData[]) { this->handleOrderExecution(packetData); });
+	messageHandlerMap.emplace(MessageType::IMBALANCE, [=](char packetData[]) { this->handleImbalance(packetData); });
+	messageHandlerMap.emplace(MessageType::ADD_ORDER_REFRESH, [=](char packetData[]) { this->handleAddOrderRefresh(packetData); });
+	messageHandlerMap.emplace(MessageType::CROSS_TRADE, [=](char packetData[]) { this->handleCrossTrade(packetData); });
+	messageHandlerMap.emplace(MessageType::NON_DISPLAYED_TRADE, [=](char packetData[]) { this->handleNonDisplayedTrade(packetData); });
+	messageHandlerMap.emplace(MessageType::CROSS_CORRECTION, [=](char packetData[]) { this->handleCrossCorrection(packetData); });
+	messageHandlerMap.emplace(MessageType::RETAIL_IMPROVEMENT, [=](char packetData[]) { this->handleRetailImprovement(packetData); });
+}
+
+void Decoder::handleTrade(char packetData[])
+{
+	TradeMessage message;
+	memcpy(&message, packetData, sizeof(TradeMessage));
+	updateOrderBook(message);
+}
+
+void Decoder::handleTradeSequenceNumberReset(char packetData[])
+{
+	SequenceNumberResetMessage message;
+	memcpy(&message, packetData, sizeof(SequenceNumberResetMessage));
+}
+
+void Decoder::handleSymbolIndexMapping(char packetData[])
+{
+	SymbolIndexMappingMessage message;
+	memcpy(&message, packetData, sizeof(SymbolIndexMappingMessage));
+	popuateSymbolIndexMap(message);
+}
+
+void Decoder::handleSymbolClear(char packetData[])
+{
+	SymbolClearMessage message;
+	memcpy(&message, packetData, sizeof(SymbolClearMessage));
+	clearSymbolIndexMap();
+}
+
+void Decoder::handleSecurityStatus(char packetData[])
+{
+	SecurityStatusMessage message;
+	memcpy(&message, packetData, sizeof(SecurityStatusMessage));
+}
+
+void Decoder::handleSourceTimeReference(char packetData[])
+{
+	SourceTimeReferenceMessage message;
+	memcpy(&message, packetData, sizeof(SourceTimeReferenceMessage));
+}
+
+void Decoder::handleQuote(char packetData[])
+{
+	QuoteMessage message;
+	memcpy(&message, packetData, sizeof(QuoteMessage));
+}
+
+void Decoder::handleAddOrder(char packetData[])
+{
+	AddOrderMessage message;
+	memcpy(&message, packetData, sizeof(AddOrderMessage));
+}
+
+void Decoder::handleModifyOrder(char packetData[])
+{
+	ModifyOrderMessage message;
+	memcpy(&message, packetData, sizeof(ModifyOrderMessage));
+}
+
+void Decoder::handleDeleteOrder(char packetData[])
+{
+	DeleteOrderMessage message;
+	memcpy(&message, packetData, sizeof(DeleteOrderMessage));
+}
+
+void Decoder::handleReplaceOrder(char packetData[])
+{
+	ReplaceOrderMessage message;
+	memcpy(&message, packetData, sizeof(ReplaceOrderMessage));
+}
+
+void Decoder::handleTradeCancel(char packetData[])
+{
+	TradeCancelMessage message;
+	memcpy(&message, packetData, sizeof(TradeCancelMessage));
+}
+
+void Decoder::handleOrderExecution(char packetData[])
+{
+	OrderExecutionMessage message;
+	memcpy(&message, packetData, sizeof(OrderExecutionMessage));
+}
+
+void Decoder::handleImbalance(char packetData[])
+{
+	ImbalanceMessage message;
+	memcpy(&message, packetData, sizeof(ImbalanceMessage));
+}
+
+void Decoder::handleAddOrderRefresh(char packetData[])
+{
+	AddOrderRefreshMessage message;
+	memcpy(&message, packetData, sizeof(AddOrderRefreshMessage));
+}
+
+void Decoder::handleCrossTrade(char packetData[])
+{
+	CrossTradeMessage message;
+	memcpy(&message, packetData, sizeof(CrossTradeMessage));
+}
+
+void Decoder::handleNonDisplayedTrade(char packetData[])
+{
+	NonDisplayedTradeMessage message;
+	memcpy(&message, packetData, sizeof(NonDisplayedTradeMessage));
+}
+
+void Decoder::handleCrossCorrection(char packetData[])
+{
+	CrossCorrectionMessage message;
+	memcpy(&message, packetData, sizeof(CrossCorrectionMessage));
+}
+
+void Decoder::handleRetailImprovement(char packetData[])
+{
+	RetailImprovementMessage message;
+	memcpy(&message, packetData, sizeof(RetailImprovementMessage));
 }
 
 void Decoder::popuateSymbolIndexMap(const SymbolIndexMappingMessage& message)
@@ -53,107 +192,18 @@ void Decoder::decodePacket(char packetData[], const int packetSize)
 
 void Decoder::handleMessage(MessageType msgType, char packetData[])
 {
-	switch (msgType)
+	auto search = messageHandlerMap.find(msgType);
+	if(search != messageHandlerMap.end())
 	{
-	case MessageType::TRADE:
-		TradeMessage tMessage;
-		memcpy(&tMessage, packetData, sizeof(TradeMessage));
-		updateOrderBook(tMessage);
-		break;
+		search->second(packetData);
+	}
+	else
+	{ 
+		bool wasInserted = unhandledMessages.insert(msgType).second;
 
-	case MessageType::SEQUENCE_NUMBER_RESET:
-		SequenceNumberResetMessage snrMessage;
-		memcpy(&snrMessage, packetData, sizeof(SequenceNumberResetMessage));
-		break;
-
-	case MessageType::SYMBOL_INDEX_MAPPING:
-		SymbolIndexMappingMessage simMessage;
-		memcpy(&simMessage, packetData, sizeof(SymbolIndexMappingMessage));
-		popuateSymbolIndexMap(simMessage);
-		break;
-
-	case MessageType::SYMBOL_CLEAR:
-		SymbolClearMessage scMessage;
-		memcpy(&scMessage, packetData, sizeof(SymbolClearMessage));
-		clearSymbolIndexMap();
-		break;
-
-	case MessageType::SECURITY_STATUS:
-		SecurityStatusMessage ssMessage;
-		memcpy(&ssMessage, packetData, sizeof(SecurityStatusMessage));
-		break;
-
-	case MessageType::SOURCE_TIME_REFERENCE:
-		SourceTimeReferenceMessage strMessage;
-		memcpy(&strMessage, packetData, sizeof(SourceTimeReferenceMessage));
-		break;
-
-	case MessageType::QUOTE:
-		QuoteMessage qMessage;
-		memcpy(&qMessage, packetData, sizeof(QuoteMessage));
-		break;
-
-	case MessageType::ADD_ORDER:
-		AddOrderMessage aoMessage;
-		memcpy(&aoMessage, packetData, sizeof(AddOrderMessage));
-		break;
-
-	case MessageType::MODIFY_ORDER:
-		ModifyOrderMessage moMessage;
-		memcpy(&moMessage, packetData, sizeof(ModifyOrderMessage));
-		break;
-
-	case MessageType::DELETE_ORDER:
-		DeleteOrderMessage doMessage;
-		memcpy(&doMessage, packetData, sizeof(DeleteOrderMessage));
-		break;
-
-	case MessageType::REPLACE_ORDER:
-		ReplaceOrderMessage roMessage;
-		memcpy(&roMessage, packetData, sizeof(ReplaceOrderMessage));
-		break;
-
-	case MessageType::ORDER_EXECUTION:
-		OrderExecutionMessage oeMessage;
-		memcpy(&oeMessage, packetData, sizeof(OrderExecutionMessage));
-		break;
-
-	case MessageType::IMBALANCE:
-		ImbalanceMessage iMessage;
-		memcpy(&iMessage, packetData, sizeof(ImbalanceMessage));
-		break;
-		
-	case MessageType::ADD_ORDER_REFRESH:
-		AddOrderRefreshMessage aorMessage;
-		memcpy(&aorMessage, packetData, sizeof(AddOrderRefreshMessage));
-		break;
-
-	case MessageType::CROSS_TRADE:
-		CrossTradeMessage ctMessage;
-		memcpy(&ctMessage, packetData, sizeof(CrossTradeMessage));
-		break;
-
-	case MessageType::NON_DISPLAYED_TRADE:
-		NonDisplayedTradeMessage ndtMessage;
-		memcpy(&ndtMessage, packetData, sizeof(NonDisplayedTradeMessage));
-		break;
-
-	case MessageType::CROSS_CORRECTION:
-		CrossCorrectionMessage ccMessage;
-		memcpy(&ccMessage, packetData, sizeof(CrossCorrectionMessage));
-		break;
-
-	case MessageType::TRADE_CANCEL:
-		TradeCancelMessage tcMessage;
-		memcpy(&tcMessage, packetData, sizeof(TradeCancelMessage));
-		break;
-
-	case MessageType::RETAIL_IMPROVEMENT:
-		RetailImprovementMessage riMessage;
-		memcpy(&riMessage, packetData, sizeof(RetailImprovementMessage));
-		break;
-
-	default:
-		throw;
+		if (wasInserted)
+		{
+			std::cout << "Unhandled Message Encountered: Message_Type " << static_cast<int>(msgType) << std::endl;
+		}
 	}
 }
--- src/cpp/WebsocketServer/source_code/Decoder/Decoder.h
+++ src/cpp/WebsocketServer/source_code/Decoder/Decoder.h
@@ -2,8 +2,11 @@
 #include <unordered_map>
 #include <memory>
 #include <string>
-#include "Model/Messages.h"
 #include <SimpleAmqpClient/SimpleAmqpClient.h>
+#include <functional>
+#include <set>
+#include "Model/Messages.h"
+#include "MessageHandler.h"
 
 struct SymbolMessage
 {
@@ -35,16 +38,43 @@ struct SymbolMessage
 
 class Decoder
 {
-private:
+private: /*Members*/
 	/*Symbol index to Symbol IndexMessage*/
+	std::set<MessageType> unhandledMessages;
+	std::unordered_map<MessageType, std::function<void(char[])>> messageHandlerMap;
 	std::unordered_map<uint32_t, SymbolIndexMappingMessage> symbolMap;
 	AmqpClient::Channel::ptr_t mqConnection;
+
+private: /*Functions*/
 	void handleMessage(MessageType msgType, char packetData[]);
 
-public:
+public: /*Functions*/
 	Decoder();
 	void decodePacket(char packetData[], const int packetSize);
+	
+private: /*Functions*/
 	void popuateSymbolIndexMap(const SymbolIndexMappingMessage& message);
 	void clearSymbolIndexMap();
 	void updateOrderBook(const TradeMessage& trade);
+	void populateMessageMap();
+
+	void handleTrade(char packetData[]);
+	void handleTradeSequenceNumberReset(char packetData[]);
+	void handleSymbolIndexMapping(char packetData[]);
+	void handleSymbolClear(char packetData[]);
+	void handleSecurityStatus(char packetData[]);
+	void handleSourceTimeReference(char packetData[]);
+	void handleQuote(char packetData[]);
+	void handleAddOrder(char packetData[]);
+	void handleModifyOrder(char packetData[]);
+	void handleDeleteOrder(char packetData[]);
+	void handleReplaceOrder(char packetData[]);
+	void handleTradeCancel(char packetData[]);
+	void handleOrderExecution(char packetData[]);
+	void handleImbalance(char packetData[]);
+	void handleAddOrderRefresh(char packetData[]);
+	void handleCrossTrade(char packetData[]);
+	void handleNonDisplayedTrade(char packetData[]);
+	void handleCrossCorrection(char packetData[]);
+	void handleRetailImprovement(char packetData[]);
 };
